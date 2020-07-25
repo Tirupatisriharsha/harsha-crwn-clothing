80d4b8c5b2921c028045dbf3a341b4b3faa2bc6f
Sriharsha Tirupati <Sriharsha.Tirupati@gisbizdev.gisbiz.chennai.com>
2020-07-25 23:38:22 +0530

    new comp added for shop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { Switch, Route } from 'react-router-dom';
 // import HomePage from './pages/homepage/homepage.component';
 import HomePage from '../src/pages/homepage/homepage.component';
+import ShopPage from '../src/pages/shop/shop.component';
 
-class App extends Component {
-  render() {
-    return <div>
-    <HomePage />
-    </div>;
+function App() {
+  
+    return (
+      <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        </Switch>
+      </div>
+    );
   }
-}
+
 
 export default App;
