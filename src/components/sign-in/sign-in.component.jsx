0ae549d7bed56c005c2ffa761897bef78ed0f39c
Sriharsha Tirupati <Sriharsha.Tirupati@gisbizdev.gisbiz.chennai.com>
2020-09-27 12:53:46 +0530

    styles applied for custom buttons

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -36,9 +36,10 @@ class SignIn extends React.Component {
                     label="Email" required/>
                     <FormInput name='password' type='password' value={this.state.password} handleChange={this.handleChange}
                     label="Password" required />
-
-                    <CustomButton type='submit'> Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> Sign In With Google </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'> Sign In </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In With Google </CustomButton>
+                    </div>
                 </form>
             </div>
         )
