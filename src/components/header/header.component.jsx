fd4cddaea23329567f698453bd081efeecaf8c95
Sriharsha Tirupati <Sriharsha.Tirupati@gisbizdev.gisbiz.chennai.com>
2020-12-27 21:07:38 +0530

    cart redux added and implemented in cart item

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import {auth} from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='header-container' to='/'>
             <Logo className='logo'/>
@@ -21,12 +23,15 @@ const Header = ({currentUser}) => (
                 :
                 <Link className='option' to='/signin'>Sign In</Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown /> }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
